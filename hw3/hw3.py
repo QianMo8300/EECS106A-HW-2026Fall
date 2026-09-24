15d17db6a927a2d4893b349bf99ab22f917db1aa
Your Name <your_email@example.com>
2026-09-23 21:50:42 -0700

    bug fixed

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -110,9 +110,9 @@ def fk_2(theta):
           partial credit.
     """
     # Specify all twists.
-    xi_1 = [0, 0, 0, 0, 0, 0]
-    xi_2 = [0, 0, 0, 0, 0, 0]
-    xi_3 = [0, 0, 0, 0, 0, 0]
+    xi_1 = [0, 0, 2, 0, 0, 1]
+    xi_2 = [-10, 0, 0, 0, 1, 0]
+    xi_3 = [0, 5, -5, 1, 0, 0]
 
     # Specify end effector configuration at theta = 0.
     gst0 = np.array([[0, -1, 0, 0],
